327c0c37b3fd20e0ed52b5de7d3576d0a01748ea
javad <javadht1990a@gmail.com>
2023-11-13 15:30:29 +0330

    complete the rest of the project and also night mode and a part of the dropdown menue

--- javascript.js
+++ javascript.js
@@ -1,18 +1,59 @@
 ///////////////////////////////
-/////////Progressbars//////////
+/////////Drop down toggle//////////
+//////////////////////////////
+
+// Get the radio button and dropdown content elements
+const tab2 = document.getElementById('tab-2');
+const cartIcon = document.querySelector('.icon.cart');
+const dropdownContent = document.querySelector('.dropdown-content');
+
+// Function to toggle the display of the dropdown content
+function toggleDropdown() {
+  if (tab2.checked) {
+    dropdownContent.style.display = 'block';
+  } else {
+    dropdownContent.style.display = 'none';
+  }
+}
+
+// Attach the function to the change event of the radio button
+tab2.addEventListener('change', toggleDropdown);
+
+// Function to hide the dropdown when clicking outside of it
+function hideDropdown(event) {
+  if (!cartIcon.contains(event.target) && !dropdownContent.contains(event.target)) {
+    dropdownContent.style.display = 'none';
+  }
+}
+
+// Attach the function to the click event of the document body
+document.body.addEventListener('click', hideDropdown);
+
+
+
+
+
+///////////////////////////////
+/////////Night Mode//////////
 //////////////////////////////
 
 
-// $(document).ready(function () {
-//     startAnimation();
-//     function startAnimation() {
-//         jQuery('.skills').each(function () {
-//             jQuery(this).find('.skillbar').animate({
-//                 width: jQuery(this).attr('data-percent')
-//             }, 4000);
-//         });
-//     }
-// });
+
+document.addEventListener('DOMContentLoaded', function () {
+      const darkModeButton = document.getElementById('theme-button');
+
+      darkModeButton.addEventListener('click', function () {
+        // Toggle dark mode class on the body
+        document.body.classList.toggle('dark-mode');
+      });
+    });
+
+
+
+
+///////////////////////////////
+/////////Progressbars//////////
+//////////////////////////////
 
 $(document).ready(function () {
     // Call startAnimation initially
